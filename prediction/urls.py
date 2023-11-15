491ca69296ebd6aae7a767d0ce98839972ec111b
amisalaam <amisalaam9@gmail>
2023-11-15 19:12:39 +0530

    venv

--- prediction/urls.py
+++ prediction/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     # JWT PATH
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
